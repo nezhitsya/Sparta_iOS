f60e25b81a3be58d03a4907b300edb2ca167aacb
nezhitsya <huyn9295@naver.com>
2021-06-29 09:50:14 +0900

    set up homework

--- week 01/Homework01/Homework01/ViewController.swift
+++ week 01/Homework01/Homework01/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var flash: UIImageView!
+    @IBOutlet weak var appTitle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        appTitle.textColor = UIColor.lightGray
+        view.backgroundColor = UIColor(red: 0.17, green: 0.25, blue: 0.30, alpha: 1.00)
     }
-
+    
+    @IBAction func touchUpOnButton(_ sender: Any) {
+        appTitle.textColor = UIColor.black
+        flash.image = UIImage(systemName: "flashlight.on.fill")
+        view.backgroundColor = UIColor(red: 0.93, green: 0.70, blue: 0.25, alpha: 1.00)
+    }
+    
+    @IBAction func touchUpOffButton(_ sender: Any) {
+        appTitle.textColor = UIColor.lightGray
+        flash.image = UIImage(systemName: "flashlight.off.fill")
+        view.backgroundColor = UIColor(red: 0.17, green: 0.25, blue: 0.30, alpha: 1.00)
+    }
+    
 }
 
