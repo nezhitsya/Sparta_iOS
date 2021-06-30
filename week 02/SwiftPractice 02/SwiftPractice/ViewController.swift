18cbc585e9cb584199ce1ec8f87c8535e908a151
nezhitsya <huyn9295@naver.com>
2021-06-30 10:26:50 +0900

    set up practice

--- week 02/SwiftPractice 02/SwiftPractice/ViewController.swift
+++ week 02/SwiftPractice 02/SwiftPractice/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textFields: [UITextField]!
+    var i = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpAutoFillButton(_ sender: Any) {
+        textFields[i].text = "안녕하세요~~😀"
+        i += 1
+    }
+    
+    @IBAction func touchUpEraseButton(_ sender: Any) {
+        for i in 0..<textFields.count {
+            var textField = textFields[i]
+            textField.text = ""
+        }
+        
+        i = 0
+    }
+    
 }
 
