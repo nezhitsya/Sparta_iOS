32f60a2d5a3adf204270b3a6810e27e7040dfe65
nezhitsya <huyn9295@naver.com>
2021-07-05 10:19:01 +0900

    set up controllers

--- week 03/SwiftPractice/SwiftPractice/InputViewController.swift
+++ week 03/SwiftPractice/SwiftPractice/InputViewController.swift
@@ -16,23 +16,31 @@ class InputViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        resultButton.layer.cornerRadius = 10
     }
     
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if var controller = segue.destination as? OutputViewController {
+            controller.numOfPeople = Int(stepper.value)
+        }
+        
     }
-    */
     
+    // stepper의 값을 textField에 보여주고, textField에 유저가 값을 텍스트로 입력 시 stepper의 value에 반영
     @IBAction func textEditingChanged(_ sender: Any) {
+        // text 가 nil 이라면 "" 디폴트 값 설정
+        var text = textField.text ?? ""
+        // text를 Double 변경 실패 시 0 디폴트 값 설정
+        stepper.value = Double(text) ?? 0
     }
     
     @IBAction func stepperValueChanged(_ sender: Any) {
+        var value = stepper.value
+        textField.text = String(Int(value))
     }
     
 }
--- week 03/SwiftPractice/SwiftPractice/OutputViewController.swift
+++ week 03/SwiftPractice/SwiftPractice/OutputViewController.swift
@@ -12,12 +12,44 @@ class OutputViewController: UIViewController {
     @IBOutlet weak var peopleLabel: UILabel!
     @IBOutlet weak var chickenLabel: UILabel!
     
+    var numOfPeople: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        peopleLabel.text = "\(numOfPeople) 명이서\n먹는다면 ..."
+        chickenLabel.text = "\(calculateChicken()) 마리를 시켜라"
     }
     
+    func calculateChicken() -> Int {
+        return fibonacci(x: numOfPeople)
+    }
+    
+    func fibonacci(x: Int) -> Int {
+        if x == 0 {
+            return 0
+        }
+        
+        if x == 1 {
+            return 1
+        }
+        
+        var a = 1
+        var b = 1
+        
+        while true {
+            var c = a + b
+            
+            if c > x {
+                return a + fibonacci(x: x - b)
+            } else if c == x {
+                return b
+            }
+            
+            a = b
+            b = c
+        }
+    }
 
     /*
     // MARK: - Navigation
