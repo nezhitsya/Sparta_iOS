6da28ed4e68be26772b91fbd30053d07b595f9a3
nezhitsya <huyn9295@naver.com>
2021-07-12 09:37:46 +0900

    set up controller

--- week 04/Homework04/Homework04/ViewController.swift
+++ week 04/Homework04/Homework04/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var catImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setImage()
+    }
+    
+    func setImage() {
+        AF.request("https://api.thecatapi.com/v1/images/search").responseJSON {
+            if let json = $0.value {
+                let data = JSON(json)
+                let image = data.arrayValue[0]["url"]
+                
+                self.catImage.sd_setImage(with: URL(string: image.stringValue), completed: nil)
+            }
+        }
+    }
 
-
+    @IBAction func imageButtonClicked(_ sender: Any) {
+        setImage()
+    }
+    
 }
 
